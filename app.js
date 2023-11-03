f262b8f7b07f4265ae0d5a77ee85dbb43ac0a32b
Ralbattah <134811738+Ralbattah@users.noreply.github.com>
2023-11-03 00:34:57 -1200

    comments and logins

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
+// import modules
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// here import mongoose and DB , const? 
 
+
+
+//import routers 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
+//express app 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+//may revise here for login authentications
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//to use routers to redirect 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
