bf3232bac1810faa49a9cf22e0b4ce90d2d6f6b2
yamat0002 <220410035@psu.edu.sa>
2023-11-12 11:55:51 +0300

    direct assessment tasks for cc & graph

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const port = process.env.PORT || 3000;
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const e = require('express');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,23 +20,60 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+
+//specifying the port
+app.listen(port);
+
+const courses = [
+  { code: 'COURSE001', name: 'Course 1', term: "223"},
+  { code: 'COURSE002', name: 'Course 2', term:"224"},
+  { code: 'COURSE003', name: 'Course 3', term: "222" },
+];
+//error. we may need to go over the diff types of errors..
+app.get('/', (req, res)=>{
+  res.render('temp', {courses})
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get('/display-course', (req, res) =>{
+  const selectedCourseCode = req.query.courseCode;
+  const selectedCourseName = courses.find(course =>
+    course.code === selectedCourseCode);
+  if (selectedCourseName){
+    const courseName = selectedCourseName.name;
+    const term = selectedCourseName.term;
+    res.render('cc', {courseName, term});
+  }
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.get('/editActivities', (req, res)=>{
+  res.render('editActivities');
+})
 
+app.post('/submit_form', (req, res)=>{
+
+  //do we parseInt or math.floor or add a logic overall to prevent decimals? what is the best?
+  const formData = {
+    project: req.body.project === 'on' ? parseInt(req.body.project_weight) : null,
+    major: req.body.major === 'on' ? parseInt(req.body.major_weight) : null,
+    assignment: req.body.assignment === 'on' ? parseInt(req.body.assignment_weight) : null,
+    quiz: req.body.quiz === 'on' ? parseInt(req.body.quiz_weight) : null,
+    final_exam: req.body.final_exam === 'on' ? parseInt(req.body.final_exam_weight) : null,
+    other: req.body.other === 'on' ? parseInt(req.body.other_weight) : null,
+  };
+
+  console.log(formData); // this is just for debugging. we'll loop over this; if value is not null, we save it in database with type, course code, and semester..
+
+ res.render('temp', { courses });// we need to add logic for after saving this info.. what is displayed later on? and if there's data saved, do we redisplay it..
+})
+
+app.get('/generate-report', (req, res)=>{
+
+
+  res.render('courseReport')
+})
+
+app.use((req, res)=>{
+  res.status(404).render('404error');
+})
 module.exports = app;
