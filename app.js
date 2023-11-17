c260c56eb08e8a056aa5533574829f44e923280a
sarh <sara.alsaleem11@gmail.com>
2023-11-17 19:09:20 +0300

    rolea ssign help pls

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+//express app
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+var chairRouter = require('./routes/chairRouter');
+
+
+app.get('/roleAssign',  (request, response) => {
+  response.render('roleAssign', { title: 'Roles' });
+});
+
+
+//app.use('/users', usersRouter);
+app.use('/roleAssign', chairRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,3 +43,7 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
---
+++ routes/chairRouter.js
@@ -0,0 +1,13 @@
+
+
+
+//creating a router to work over here
+const express = require('express');
+const router = express.Router();
+const chairController = require('../controller/chairController');
+
+
+router.post('/roleAssign');
+
+
+module.exports = router;
