abb8bb9f46f30edf9c2bee920950c95007ec3e2e
yamat0002 <220410035@psu.edu.sa>
2023-11-28 02:26:49 +0300

    home cc, home ci (chair & dcc & qa left)

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,12 +4,24 @@ async function getUser(req,res ){
     
     const user= req.session.user;
     const term = await userModel.getCurrentTerm(user.username);
-    console.log("termmmmmm", term)
+    const userRoles = await userModel.getUserRoles(user.username, term);
+    const coordinatedCourses = await userModel.getCoordinatedCourses(user.username, term);
+    
+    console.log("username", user.username)
+    console.log("term", term)
     const fullName = user.fullName;
     const title = "Miqyas: Home";
+    const courses = await userModel.getCourses(user.username, term);
+
+    console.log(courses);
+    console.log("coordinated courses", coordinatedCourses);
+    console.log("user roles", userRoles);
+    
     console.log("lolgetuser", user)
-    res.render('home', {user, title, fullName, term});
+    res.render('home', {user, title, fullName, term, courses, userRoles, coordinatedCourses});
 }
+
+
 
 
   module.exports = {getUser};
--- models/UserModel.js
+++ models/UserModel.js
@@ -23,14 +23,61 @@ async function getUserByUsername(username) {
     try {
         const [rows] = await pool.execute(sql, [username]);
         const semesters = rows.map(row => row.semester);
-        return semesters;
+        return semesters[0];
     } catch (error) {
         console.error('Error fetching semester: ', error);
         throw error;
     }
 }
 
+async function getCourses(username, semester) {
+    const sql = `
+        SELECT cs.courseCode, cs.sectionNumber, cs.semester, c.courseName
+        FROM course_section cs
+        JOIN course c ON cs.courseCode = c.courseCode
+        WHERE cs.username = ? AND cs.semester = ?`;
+
+    try {
+        const [rows] = await pool.execute(sql, [username, semester]);
+        return rows;
+    } catch (error) {
+        console.error('Error fetching courses with names', error);
+        throw error;
+    }
+}
+
+async function getUserRoles(username, semester) {
+    const sql = `SELECT DISTINCT role FROM faculty_role WHERE username = ? AND semester = ?`;
+
+    try {
+        const [rows] = await pool.execute(sql, [username, semester]);
+        const roles = rows.map(row => row.role);
+        return roles;
+    } catch (error) {
+        console.error('Error fetching user roles: ', error);
+        throw error;
+    }
+}
+
+async function getCoordinatedCourses(username, semester) {
+    const sql = `
+        SELECT c.courseCode, cr.courseName
+        FROM coordinator c
+        JOIN course cr ON c.courseCode = cr.courseCode
+        WHERE c.username = ? AND c.semester = ?
+    `;
+
+    try {
+        const [rows] = await pool.execute(sql, [username, semester]);
+        return rows; // Assuming a coordinator is assigned to only one course
+    } catch (error) {
+        console.error('Error fetching coordinated course: ', error);
+        throw error;
+    }
+}
 
 
-module.exports ={ getUserByUsername, getCurrentTerm};
+
+
+module.exports ={ getUserByUsername, getCurrentTerm, getCourses, getUserRoles, getCoordinatedCourses};
 
